e07664b118f7c42f631e2766c9a700df8f3f82f3
Im Chaewon <lisa425@sookmyung.ac.kr>
2024-03-10 13:03:48 +0900

    컴포넌트 구현 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import styled from '@emotion/styled'
 import './App.css'
 import Text from '@shared/Text'
 import Button from './components/shared/Button'
-
+import Input from './components/shared/Input'
+import TextField from '@shared/TextField'
+import Alert from './components/shared/Alert'
+import { useAlertContext } from './contexts/AlertContext'
 function App() {
+  const { open } = useAlertContext()
   return (
     <div className="App">
       <Text typography="t1" display="block" color="blue">
@@ -26,6 +30,27 @@ function App() {
       </Button>
       <Button weak={true}>클릭</Button>
       <Button disabled={true}>클릭</Button>
+      <div style={{ height: 10, width: '100%', background: '#efefef' }} />
+      <TextField label="아이디" />
+      <Input placeholder="아이디를 입력하세요" aria-invalid={true} />
+      <Input placeholder="비밀번호를 입력하세요" />
+
+      {/* <Alert
+        open={true}
+        title="알럿이 떴습니다."
+        onButtonClick={() => {}}
+        description="안녕하세요"
+      /> */}
+
+      <Button
+        onClick={() => {
+          open({
+            title: '카드신청완료',
+            description: '내역페이지에서 확인해주세요',
+            onButtonClick: () => {},
+          })
+        }}
+      />
     </div>
   )
 }
